f4d4a480b8945bded727cf5fb5c8e80152bdc53e
almedaspani <almedaspani@gmail.com>
2021-02-16 14:43:35 +0000

    User verification Email added

--- cardiomyopathy-app/src/composables/useSignup.js
+++ cardiomyopathy-app/src/composables/useSignup.js
@@ -15,7 +15,9 @@ const signup = async (email, password, displayName) => {
 		// you cannot create the properties, you have to use already created
 		await res.user.updateProfile({ displayName })
 		error.value = null
-
+		
+		// send an email verification to the users email
+		res.user.sendEmailVerification();
 		console.log(res.user)
 
 		return res
