be07f0b5900605665a133898197abc4d0b917d3c
TaintedGalaxy <mohambad@hotmail.com>
2021-02-22 18:15:01 +0000

    Resolved wrong beforeEnter variable

--- cardiomyopathy-app/src/router/index.js
+++ cardiomyopathy-app/src/router/index.js
@@ -50,7 +50,7 @@ const routes = [
     path: '/dashboard',
     name: 'Dashboard',
     component: Dashboard,
-    beforeEnter: authenticationGuard
+    beforeEnter: loggedOutGuard
   },
   {
     path: '/chart-example',
