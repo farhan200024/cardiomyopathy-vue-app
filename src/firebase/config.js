69e0f0952d7598dc240c71475df5d15754888e61
Farhan Muhammad <farhan.aslam330@gmail.com>
2021-03-08 16:45:02 +0000

    firebase hosting setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import 'firebase/auth'
 
 //adding the firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDfYBgvCS1Wu2MIpUaCqObkrG5bpq87WmM",
-  authDomain: "cardiomyopathy-auth.firebaseapp.com",
-  projectId: "cardiomyopathy-auth",
-  storageBucket: "cardiomyopathy-auth.appspot.com",
-  messagingSenderId: "648071341133",
-  appId: "1:648071341133:web:b3083b0b1f2ddb6deaa804"
+  apiKey: "AIzaSyCGCWBCLfWOgBCB2-nBEHcJcyT1EUHD1UE",
+  authDomain: "cardiomyopathy-farhan.firebaseapp.com",
+  projectId: "cardiomyopathy-farhan",
+  storageBucket: "cardiomyopathy-farhan.appspot.com",
+  messagingSenderId: "262113426024",
+  appId: "1:262113426024:web:4bb490ad73ecf40aa919dd",
 };
 
 // initializing the firebase configuration
